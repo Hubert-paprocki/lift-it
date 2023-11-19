e13920dd9db05d0044deef90ac3ea0758a527351
Hubert Paprocki <hubertpaprocki.dev@gmail.com>
2023-11-19 11:49:26 +0100

    feat:add app feature list imgs

--- src/components/appFeaturesList/AppFeaturesList.tsx
+++ src/components/appFeaturesList/AppFeaturesList.tsx
@@ -11,23 +11,23 @@ function AppFeaturesList() {
   const appFeatureList: Feature[] = [
     {
       featureName: "Lift Weight Tracker",
-      featurePicture: "url13.jpg",
+      featurePicture: "liftingWeightProgress.png",
       featureDesc:
         "Record your weights, reps per exercise, and showcase your lifting achievements.",
     },
     {
       featureName: "Lift Plan",
-      featurePicture: "url13.jpg",
+      featurePicture: "liftPlan.png",
       featureDesc: "Never wonder 'what's next?' with a saved workout plan.",
     },
     {
       featureName: "Water Counter",
-      featurePicture: "url13.jpg",
+      featurePicture: "water.png",
       featureDesc: "Stay hydrated by keeping track of your daily water intake.",
     },
     {
       featureName: "Body Weight Progress",
-      featurePicture: "url13.jpg",
+      featurePicture: "bodyWeightProgress.png",
       featureDesc:
         "Monitor your weight to assess the effectiveness of your diet.",
     },
